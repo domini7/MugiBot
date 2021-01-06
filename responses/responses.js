614a423a58a0ab35ed555fffe42c7d7de9dc35ad
domini7 <67447432+domini7@users.noreply.github.com>
2021-01-06 02:41:36 -0800

    Removing lines from responses.js

--- responses/responses.js
+++ responses/responses.js
@@ -1,16 +1,4 @@
-const responseObject = {
-	ayy: "ayy lmao",
-	wat: "say what",
-	lol: "roflmaotntpmp",
-};
-
-exports.responseObject = responseObject;
-
 const reactObject = {
-	miguel: "🇲🇽",
-	lamo: "😎",
-	howey: "👨‍❤️‍💋‍👨",
-	daniel: "👨‍❤️‍💋‍👨",
 	mugi: "🤖",
 	bot: "🤖",
 	ok: "🆗",
