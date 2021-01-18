2bb79bb87d5476383abda3d3063be7dc13770906
domini7 <67447432+domini7@users.noreply.github.com>
2021-01-17 18:13:24 -0800

    Commas

--- commands/covid.js
+++ commands/covid.js
@@ -4,6 +4,8 @@ module.exports = {
 	name: "covid",
 	description: "Check CoronaVirus stats",
 	execute(message, args, Discord) {
+
+		// if there is no arg, command will search global
 		let search = "country";
 		if (!args.length) {
 			search = "totals";
@@ -11,7 +13,7 @@ module.exports = {
 
 		const req = unirest(
 			"GET",
-			"https://covid-19-data.p.rapidapi.com/" + search,
+			"https://covid-19-data.p.rapidapi.com/" + search
 		);
 
 		const word = args.join(" ");
@@ -33,7 +35,7 @@ module.exports = {
 				const stats = res.body[0];
 				const date = stats.lastUpdate.slice(0, 10);
 				let country;
-				!args.length ? country = "World" : country = stats.country;
+				!args.length ? (country = "World") : (country = stats.country);
 
 				const newEmbed = new Discord.MessageEmbed()
 					.setColor("#FFA500")
@@ -41,29 +43,31 @@ module.exports = {
 					.addFields(
 						{
 							name: "Cases",
-							value: `${stats.confirmed}`,
+							value: `${stats.confirmed.toLocaleString()}`,
 							inline: true,
 						},
 						{
 							name: "Recovered",
-							value: `${stats.recovered}`,
+							value: `${stats.recovered.toLocaleString()}`,
 							inline: true,
 						},
 						{
 							name: "Active",
-							value: `${stats.confirmed - stats.recovered}`,
+							value: `${(stats.confirmed - stats.recovered).toLocaleString()}`,
 							inline: true,
 						},
 						{
 							name: "Deaths",
-							value: `${stats.deaths}`,
+							value: `${stats.deaths.toLocaleString()}`,
 							inline: true,
 						}
 					)
 					.setFooter(`Accurate as of: ${date}`);
 				message.channel.send(newEmbed);
 			} catch (error) {
-				message.reply("Can't find data for that country! Maybe spell propery? Thanks.");
+				message.reply(
+					"Can't find data for that country! Maybe spell propery? Thanks."
+				);
 			}
 		});
 	},
