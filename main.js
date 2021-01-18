e3e76c8517a302f49f9adf46de52c9eddac6c6d8
domini7 <67447432+domini7@users.noreply.github.com>
2021-01-17 23:15:59 -0800

    Removed two unnecessary lines

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs");
-
-const rps = require("./responses/responses.js");
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
