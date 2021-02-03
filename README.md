39fab22e4ef0f5a1f2135e49a816862ece878d15
domini7 <67447432+domini7@users.noreply.github.com>
2021-02-03 03:46:39 -0800

    Re-worked help command

--- README.md
+++ README.md
@@ -5,24 +5,21 @@
 
 This is a multi-purpose personal Discord bot built using JavaScript. DM's are open for commands at MugiBot#9444
 
-## General Commands
+## Commands
+
+## General
 
 ```
 m-help                  | DM's you the list of commands.
-m-play                  | Play music from title or youtube URL (Must be in a voice channel) (No queue system).
-m-stop                  | Stop current music.
 m-image (keyword)       | Get a random image based off keyword.
 m-inspirobot            | Bot sends an inspirational image from inspirobot.
 m-covid (optional)      | Get coronavirus stats for the world or specific countries.
 m-define (keyword)      | Define a word using urban dictionary. Potentially NSFW
 m-flip                  | Flip a coin.
 m-userinfo              | Displays basic user info, must be used in a server.
-m-echo                  | Bot repeats what is said after 'm-echo ...'.
-m-gpt2 [sentence]       | Bot tries to finish your sentence, responds within a minute.
-m-gpt2xl [sentence]     | Same function as m-gpt2, but with a much larger model. Takes up to 5-10 minutes to respond.
 m-version               | Checks current version and date last updated.
 ```
-## NBA Commands
+## NBA
 
 ```
 m-pstats (name) [year]  | Check a NBA player's basic season stats with season as optional arg.
@@ -32,4 +29,17 @@ m-plastgame (name)      | Check a player's last game.
 m-tstats (team) [year]  | Check a NBA team's current or *optionally* past season stats.
 m-standings [year]      | Check current NBA standings or optionally check past seasons.
 m-leaders [year]        | Displays league leaders.
+```
+## Music
+
+```
+m-play                  | Play music from title or youtube URL (Must be in a voice channel) (No queue system).
+m-stop                  | Stop current music.
+```
+## Bot Commands
+
+```
+m-echo                  | Bot repeats what is said after 'm-echo ...'.
+m-gpt2 [sentence]       | Bot tries to finish your sentence, responds within a minute.
+m-gpt2xl [sentence]     | Same function as m-gpt2, but with a much larger model. Takes up to 5-10 minutes to respond.
 ```
--- commands/bot/help.js
+++ commands/bot/help.js
@@ -1,21 +1,42 @@
 module.exports = {
 	name: "help",
 	description: "Displays public commands",
-	cooldown: 120,
+	cooldown: 10,
 	execute(client, message, args, Discord) {
-		const newEmbed = new Discord.MessageEmbed()
+		const help = new Discord.MessageEmbed()
 			.setColor("#808080")
 			.setThumbnail("https://i.imgur.com/MINhF0h.jpg")
 			.setTitle("MugiBot")
 			.setURL("https://github.com/domini7/MugiBot")
-			.setDescription("General Commands")
+			.setDescription(
+				"Open-source Discord.js bot\n\nTo get info about specific categories,\ndo `m-help <category>`.\nTo show all commands, do `m-help all.`"
+			)
 			.addFields(
 				{
-					name: "**`m-play/stop (direct URL or title)`**",
-					value:
-						"Play and stop music (Must be in voice channel for both) (No queue system yet).",
+					name: "**General**",
+					value: "Commands that don't fit into specific categories.",
 					inline: false,
 				},
+				{
+					name: "**NBA**",
+					value: "NBA stat commands.",
+					inline: false,
+				},
+				{
+					name: "**Music**",
+					value: "Music commands.",
+					inline: false,
+				},
+				{
+					name: "**Bot**",
+					value: "Commands to make the bot say something.",
+					inline: false,
+				}
+			);
+
+		const general = new Discord.MessageEmbed()
+			.setColor("#808080")
+			.addFields(
 				{
 					name: "**`m-image (keyword)`**",
 					value: "Get a random image based off keyword.",
@@ -27,13 +48,15 @@ module.exports = {
 					inline: false,
 				},
 				{
-					name: "**`m-covid (OPTIONAL: country name)`**",
-					value: "Get coronavirus stats for the world or specific countries.",
+					name: "**`m-define (word)`**",
+					value:
+						"Define a word using UrbanDictionary, potentially offensive.",
 					inline: false,
 				},
 				{
-					name: "**`m-define (keyword)`**",
-					value: "Define a word using urban dictionary. Potentially NSFW.",
+					name: "**`m-covid (OPTIONAL: country name)`**",
+					value:
+						"Get coronavirus stats for the world or specific countries.",
 					inline: false,
 				},
 				{
@@ -47,68 +70,113 @@ module.exports = {
 					inline: false,
 				},
 				{
-					name: "**`m-echo`**",
-					value: "Bot repeats what you say after `m-echo`.",
-					inline: false,
-				},
-				{
-					name: "**`m-gpt2 [sentence]`**",
-					value: "Bot tries to finish your sentence. Processes under a minute.",
-					inline: false,
-				},
-				{
-					name: "**`m-gpt2xl [sentence]`**",
-					value: "Same function as m-gpt2 with bigger model. Can take up to 5-10 min.",
-					inline: false,
-				},
-				{
 					name: "**`m-version`**",
-					value: "Checks current version and date last updated.",
-					inline: false,
-				},
-			);
-		const nbacommands = new Discord.MessageEmbed()
-			.setColor("#808080")
-			.setTitle("NBA Commands")
-			.addFields(
-				{
-					name: "**`m-pstats (player name) [OPTIONAL YEAR]`**",
-					value: "Check a NBA player's basic season stats.",
-					inline: false,
-				},
-				{
-					name: "**`m-pstats-per36`**",
-					value: "Same as pstats but with per 36 min numbers.",
-					inline: false,
-				},
-				{
-					name: "**`m-pbio (player name)`**",
-					value: "Check a NBA player's basic info.",
-					inline: false,
-				},
-				{
-					name: "**`m-plastgame (player name)`**",
-					value: "Check a player's last game. (Can't check players who didn't play their team's last game!)",
-					inline: false,
-				},
-				{
-					name: "**`m-tstats (team) [OPTIONAL YEAR]`**",
-					value: "Displays basic NBA team stats.",
-					inline: false,
-				},
-				{
-					name: "**`m-standings [OPTIONAL YEAR]`**",
-					value: "Displays current NBA standings.",
-					inline: false,
-				},
-				{
-					name: "**`m-leaders [OPTIONAL YEAR]`**",
-					value: "Displays league leaders.",
+					value: "Displays version and date MugiBot was last updated.",
 					inline: false,
 				}
 			);
 
-		message.author.send(newEmbed);
-		message.author.send(nbacommands);
+		const nba = new Discord.MessageEmbed().setColor("#808080").addFields(
+			{
+				name: "**`m-pstats (player name) [OPTIONAL YEAR]`**",
+				value: "Check a NBA player's basic season stats.",
+				inline: false,
+			},
+			{
+				name: "**`m-pstats-per36`**",
+				value: "Same as pstats but with per 36 min numbers.",
+				inline: false,
+			},
+			{
+				name: "**`m-pbio (player name)`**",
+				value: "Check a NBA player's basic info.",
+				inline: false,
+			},
+			{
+				name: "**`m-plastgame (player name)`**",
+				value:
+					"Check a player's last game. (Can't check players who didn't play their team's last game!)",
+				inline: false,
+			},
+			{
+				name: "**`m-tstats (team) [OPTIONAL YEAR]`**",
+				value: "Displays basic NBA team stats.",
+				inline: false,
+			},
+			{
+				name: "**`m-standings [OPTIONAL YEAR]`**",
+				value: "Displays current NBA standings.",
+				inline: false,
+			},
+			{
+				name: "**`m-leaders [OPTIONAL YEAR]`**",
+				value: "Displays league leaders.",
+				inline: false,
+			}
+		);
+
+		const music = new Discord.MessageEmbed().setColor("#808080").addFields(
+			{
+				name: "**`m-play (youtube URL or title)`**",
+				value: "Play music. (Must be in voice channel)",
+				inline: false,
+			},
+			{
+				name: "**`m-stop`**",
+				value: "Bot stops currently playing music and leaves channel.",
+				inline: false,
+			}
+		);
+
+		const bot = new Discord.MessageEmbed().setColor("#808080").addFields(
+			{
+				name: "**`m-echo`**",
+				value: "Bot repeats what you say after `m-echo`.",
+				inline: false,
+			},
+			{
+				name: "**`m-gpt2 [sentence]`**",
+				value:
+					"Bot tries to finish your sentence. Processes under a minute.",
+				inline: false,
+			},
+			{
+				name: "**`m-gpt2xl [sentence]`**",
+				value:
+					"Same function as m-gpt2 with bigger model. Can take up to 5-10 min.",
+				inline: false,
+			}
+		);
+
+		const dm = message.author;
+
+		if (!args.length) {
+			dm.send(help);
+		} else {
+			const search = args[0].toLowerCase();
+
+			switch (search) {
+				case "general":
+					dm.send(general);
+					break;
+				case "nba":
+					dm.send(nba);
+					break;
+				case "music":
+					dm.send(music);
+					break;
+				case "bot":
+					dm.send(bot);
+					break;
+				case "all":
+					dm.send(general);
+					dm.send(nba);
+					dm.send(music);
+					dm.send(bot);
+					break;
+				default:
+					dm.send("Couldn't find that category! (Check spelling?)");
+			}
+		}
 	},
 };
