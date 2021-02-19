db8c2794b023e6230b320c0665f35a3d887d10f5
domini7 <67447432+domini7@users.noreply.github.com>
2021-02-19 08:39:58 -0800

    lottery

--- commands/general/score.js
+++ commands/general/score.js
@@ -3,9 +3,11 @@ const stringSimilarity = require("string-similarity");
 const { rnd } = require("../../util/Utils.js");
 const fs = require("fs");
 
+const lotteryCooldown = new Set();
+
 module.exports = {
 	name: "score",
-	aliases: ["leaderboard", "manage", "buyin"],
+	aliases: ["leaderboard", "manage", "buyin", "lottery"],
 	cooldown: 20,
 	execute(client, message, args, Discord, cmd) {
 		const bbgm = bbgmDiscord["bbgmDiscord"];
@@ -112,6 +114,60 @@ module.exports = {
 
 			bbgm[user] = 5;
 			message.reply("Your points have been set back to 5");
+
+			fs.writeFile(
+				"../MugiBot/assets/json/bbgm.json",
+				JSON.stringify(bbgmDiscord),
+				(error) => {
+					if (error) console.log(error);
+				}
+			);
+		}
+
+		if (cmd === "lottery" && message.channel.name === "bot-spam") {
+			const user = message.author.username;
+
+			if (lotteryCooldown.has(message.author.id)) {
+				message.reply(
+					"Wait a minute before trying to play the lottery"
+				);
+				return;
+			}
+
+			if (bbgm[user] < 5) {
+				return message.reply("You need 5 points to play the lottery, use `m-buyin` to get 5 points");
+			}
+
+			if (Math.random() > 0.99) {
+				bbgm[user] += +bbgmDiscord["lottery"];
+				message.reply(
+					`you win **${bbgmDiscord["lottery"]}** points! New score: **${bbgm[user]}**`
+				);
+				message.channel.send("Lottery pool has been reset to 100");
+				bbgmDiscord["lottery"] = 100;
+			} else {
+				bbgm[user] -= 5;
+				bbgmDiscord["lottery"] += 5;
+				message.reply(
+					"you didn't win! You lose **5** points. Try again in a minute."
+				);
+				message.channel.send(
+					`Lottery pool is now: ${bbgmDiscord["lottery"]}`
+				);
+			}
+
+			lotteryCooldown.add(message.author.id);
+			setTimeout(() => {
+				lotteryCooldown.delete(message.author.id);
+			}, 60000);
+
+			fs.writeFile(
+				"../MugiBot/assets/json/bbgm.json",
+				JSON.stringify(bbgmDiscord),
+				(error) => {
+					if (error) console.log(error);
+				}
+			);
 		}
 
 		if (cmd === "manage" && message.author.id === "188530356394131456") {
@@ -137,14 +193,18 @@ module.exports = {
 			if (args[0] === "take") {
 				bbgm[user] -= num;
 				message.channel.send(
-					`${user}'s points have been decreased to **${rnd(bbgm[user])}**`
+					`${user}'s points have been decreased to **${rnd(
+						bbgm[user]
+					)}**`
 				);
 			}
 
 			if (args[0] === "give") {
 				bbgm[user] += +num;
 				message.channel.send(
-					`${user}'s points have been increased to **${rnd(bbgm[user])}**`
+					`${user}'s points have been increased to **${rnd(
+						bbgm[user]
+					)}**`
 				);
 			}
 
