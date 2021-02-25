e745bb5153f432ccf045cff309ad169a2b3defb9
domini7 <67447432+domini7@users.noreply.github.com>
2021-02-25 09:38:40 -0800

    suggestion command

---
+++ commands/general/suggest.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: "suggest",
+	descriptions: "Sends a suggestion to my suggestion channel",
+	execute(client, message, args, Discord) {
+		if (!args.length) return message.reply("you need to type something");
+
+		const suggestion = args.join(" ");
+
+		const embed = new Discord.MessageEmbed()
+			.setColor("800080")
+			.setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+			.setDescription(suggestion);
+
+		client.channels.cache.get('814549161088909372').send(embed);
+	},
+};
