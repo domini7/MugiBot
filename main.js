5ce19c30966d902190c181bda4d28b16b3158951
domini7 <67447432+domini7@users.noreply.github.com>
2021-02-26 09:10:27 -0800

    Lint main

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
-require('dotenv').config();
+require("dotenv").config();
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 
-['commandHandler', 'eventHandler'].forEach(handler =>{
+["commandHandler", "eventHandler"].forEach(handler =>{
 	require(`./handlers/${handler}`)(client, Discord);
 });
 
