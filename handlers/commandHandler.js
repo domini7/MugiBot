7dcf6569c1385e4ecc67ad642c0263d8b48dbca4
domini7 <67447432+domini7@users.noreply.github.com>
2021-02-26 08:51:05 -0800

    Lint handlers

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-module.exports = (client, Discord) => {
+module.exports = (client) => {
 	const generalCommandFiles = fs
 		.readdirSync("./commands/general/")
 		.filter((file) => file.endsWith(".js"));
