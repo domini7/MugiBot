277e42a303940e33453959c80a318f376668a628
domini7 <67447432+domini7@users.noreply.github.com>
2021-02-27 07:18:11 -0800

    run script

--- src/commands/general/version.js
+++ src/commands/general/version.js
@@ -4,8 +4,8 @@ module.exports = {
 	cooldown: 500,
 	execute(client, message, args, Discord) {
 		const newEmbed = new Discord.MessageEmbed().addFields(
-			{ name: "Version", value: "0.5.3", inline: true },
-			{ name: "Last updated", value: "Feb 25th, 2021", inline: true }
+			{ name: "Version", value: "0.5.4", inline: true },
+			{ name: "Last updated", value: "Feb 27th, 2021", inline: true }
 		);
 
 		message.channel.send(newEmbed);
