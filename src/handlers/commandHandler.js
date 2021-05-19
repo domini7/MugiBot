9026b7e761a400e36bb73f3d55a26ca416422578
domini7 <67447432+domini7@users.noreply.github.com>
2021-05-18 21:08:53 -0700

    Refactor (DRY)

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,81 +1,26 @@
 const fs = require("fs");
 
 module.exports = (client) => {
-	const generalCommandFiles = fs
-		.readdirSync("./src/commands/general/")
-		.filter((file) => file.endsWith(".js"));
+	// Looping through every folder and file in ./commands
+	for (const commandFiles of [
+		"bot",
+		"games",
+		"general",
+		"music",
+		"nba",
+		"search",
+	]) {
+		const generalCommandFiles = fs
+			.readdirSync(`./src/commands/${commandFiles}/`)
+			.filter((file) => file.endsWith(".js"));
 
-	for (const file of generalCommandFiles) {
-		const command = require(`../commands/general/${file}`);
-		if (command.name) {
-			client.commands.set(command.name, command);
-		} else {
-			continue;
-		}
-	}
-
-	const nbaCommandFiles = fs
-		.readdirSync("./src/commands/nba/")
-		.filter((file) => file.endsWith(".js"));
-
-	for (const file of nbaCommandFiles) {
-		const command = require(`../commands/nba/${file}`);
-		if (command.name) {
-			client.commands.set(command.name, command);
-		} else {
-			continue;
-		}
-	}
-
-	const musicCommandFiles = fs
-		.readdirSync("./src/commands/music/")
-		.filter((file) => file.endsWith(".js"));
-
-	for (const file of musicCommandFiles) {
-		const command = require(`../commands/music/${file}`);
-		if (command.name) {
-			client.commands.set(command.name, command);
-		} else {
-			continue;
-		}
-	}
-
-	const botCommandFiles = fs
-		.readdirSync("./src/commands/bot/")
-		.filter((file) => file.endsWith(".js"));
-
-	for (const file of botCommandFiles) {
-		const command = require(`../commands/bot/${file}`);
-		if (command.name) {
-			client.commands.set(command.name, command);
-		} else {
-			continue;
-		}
-	}
-
-	const searchCommandFiles = fs
-		.readdirSync("./src/commands/search/")
-		.filter((file) => file.endsWith(".js"));
-
-	for (const file of searchCommandFiles) {
-		const command = require(`../commands/search/${file}`);
-		if (command.name) {
-			client.commands.set(command.name, command);
-		} else {
-			continue;
-		}
-	}
-
-	const gamesCommandFiles = fs
-		.readdirSync("./src/commands/games/")
-		.filter((file) => file.endsWith(".js"));
-
-	for (const file of gamesCommandFiles) {
-		const command = require(`../commands/games/${file}`);
-		if (command.name) {
-			client.commands.set(command.name, command);
-		} else {
-			continue;
+		for (const file of generalCommandFiles) {
+			const command = require(`../commands/${commandFiles}/${file}`);
+			if (command.name) {
+				client.commands.set(command.name, command);
+			} else {
+				continue;
+			}
 		}
 	}
 };
