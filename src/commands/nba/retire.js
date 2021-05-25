e9491da659d0a2549c972a10b9387bf96c148e53
domini7 <67447432+domini7@users.noreply.github.com>
2021-05-24 20:22:29 -0700

    Refactor

--- src/commands/nba/retire.js
+++ src/commands/nba/retire.js
@@ -16,38 +16,23 @@ module.exports = {
 		// Takes in arg and seaches for that player
 		let player = args.join(" ");
 
-		let lastArg = args[args.length - 1];
-
 		let season = "2020-21";
 
-		// This is all for a season search, takes last arg if its a number and converts it into proper seasonId
+		let lastArg = args[args.length - 1];
+		// Check if a player is searching for a season
 		if (!isNaN(lastArg)) {
-			// nameOnly will be used to seperate the arg searching for a name from the year
-			let nameOnly = player.length - lastArg.length - 1;
-
+			// Seperate searched player from year
+			const nameOnly = player.length - lastArg.length - 1;
 			player = player.slice(0, nameOnly);
 
-			// convert season arg into a number to subtract from
-			let num0 = parseInt(lastArg);
+			if (lastArg < 1999)
+				return message.reply("Can't search for years before 1999");
+			// String because it's not detected as int
+			if (lastArg === "2000")
+				return message.reply("Can't search for stats in 2000");
+			if (lastArg > 2021) lastArg = "2021";
 
-			if (num0 < 1999)
-				return message.reply(
-					"Cant search for years before 1999, sorry!"
-				);
-
-			if (num0 === 2000)
-				return message.reply("Cant search for stats in 2000, sorry!");
-
-			if (num0 > 2021)
-				return message.reply("That season hasn't happened yet!");
-
-			const num1 = lastArg;
-
-			let num2 = num0 - 1;
-
-			toString(num2);
-
-			season = num2 + "-" + num1.slice(num1.length - 2);
+			season = seasonString(lastArg);
 		}
 
 		try {
