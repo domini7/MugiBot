a313b8712e42bd4ba1bade62fff465a367f3093f
domini7 <67447432+domini7@users.noreply.github.com>
2021-07-22 13:16:22 -0700

    Merge gpt files

--- src/commands/bot/gpt2.js
+++ src/commands/bot/gpt2.js
@@ -2,10 +2,10 @@ const booste = require("booste");
 
 module.exports = {
 	name: "gpt2",
-	aliases: ["gpt"],
+	aliases: ["gpt", "gpt2xl", "xl"],
 	description: "Predict end of text",
 	cooldown: 120,
-	async execute(client, message, args) {
+	async execute(client, message, args, Discord, cmd) {
 		let inString = "I";
 
 		if (args.length) {
@@ -13,14 +13,16 @@ module.exports = {
 		}
 
 		try {
-			const outList = await booste.gpt2(process.env.BOOSTE, inString, 40);
+			let outList;
+			const gpt2 = ["gpt2", "gpt"];
+
+			if (gpt2.includes(cmd)) {
+				outList = await booste.gpt2(process.env.BOOSTE, inString, 45);
+			} else {
+				outList = await booste.gpt2XL(process.env.BOOSTE, inString, 37);
+			}
 
 			let outString = outList.join(" ");
-			console.log(outString);
-
-			// Hack to cut off unfinished sentences
-			outString = outString.substring(0, outString.lastIndexOf(".") + 1);
-
 			message.channel.send(`${inString}` + " " + `${outString}`);
 		} catch (error) {
 			console.error(error);
