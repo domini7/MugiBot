fec9ea1924ce1b45af78562e2bddf6074e2e16e2
domini7 <67447432+domini7@users.noreply.github.com>
2021-09-23 12:28:06 -0700

    Upgrade reddit command

--- src/commands/search/reddit.js
+++ src/commands/search/reddit.js
@@ -11,76 +11,90 @@ module.exports = {
 		const subreddit = args[0] ?? "all";
 
 		// How old can the posts be in hours
-		const maxAgeInHours = args[1] ?? 4.5;
+		const maxAgeInHours = args[1] ?? 4.3;
 
 		// Max # of comments on a post
-		const maxComments = args[2] ?? 110;
+		const maxComments = args[2] ?? 100;
 
 		// Top comment has to be getting this many upvotes per min to qualify
-		const minUpvotesPerMin = args[3] ?? 0.8;
+		const minUpvotesPerMin = args[3] ?? 1;
+
+		// How many pages to go through
+		let iterateCount = args[4] ?? 8;
 
 		// Reducing search count to prevent a weird issue with older post
-		let searchCount = 80;
+		let searchCount = 100;
 		if (subreddit != "all") {
-			searchCount = 30;
+			searchCount = 28;
+			iterateCount = 1;
 		}
 
-		const seconds = Date.now() / 1000;
-
 		try {
-			// Gets the best 100 post on r/all, can't search for more sadly.
-			const { body } = await request.get(
-				`https://www.reddit.com/r/${subreddit}.json?limit=${searchCount}`
-			);
+			// This is used so the request can page through r/all and get more posts
+			let lastLink = "";
 
-			const info = body.data.children;
-
-			// Weed out any post that don't pass given filters.
-			const filteredData = info.filter(function (item) {
-				return (
-					item.data.num_comments <= maxComments &&
-					item.data.created_utc >= seconds - maxAgeInHours * 3600 &&
-					item.data.locked === false
-				);
-			});
-
-			if (!filteredData.length)
-				return message.reply("Couldn't find any links!");
-
+			// Filtered links sent here
 			let potentialPost = [];
-			for (let i = 0; i < filteredData.length; i++) {
-				potentialPost.push(
-					`https://reddit.com${filteredData[i].data.permalink}`
-				);
+
+			const seconds = Date.now() / 1000;
+			for (let i = 0; i < iterateCount; i++) {
+				const { body } = await request
+					.get(`https://www.reddit.com/r/${subreddit}.json`)
+					.query({
+						limit: searchCount,
+						after: lastLink,
+					});
+
+				const info = body.data.children;
+
+				lastLink = info[info.length - 1].data.name;
+
+				const filteredData = info.filter(function (item) {
+					return (
+						item.data.num_comments <= maxComments &&
+						item.data.created_utc >=
+							seconds - maxAgeInHours * 3600 &&
+						item.data.locked === false
+					);
+				});
+				for (let i = 0; i < filteredData.length; i++) {
+					potentialPost.push(
+						`https://reddit.com${filteredData[i].data.permalink}`
+					);
+				}
 			}
 
 			let qualifiedPost = [];
 			// This loop will be checking the top comment for qualifications.
 			for (const post of potentialPost) {
-				const { body } = await request.get(`${post}.json`);
+				try {
+					const { body } = await request.get(`${post}.json`);
 
-				const checkForDistinguished =
-					body[1].data.children[0].data.distinguished;
+					const checkForDistinguished =
+						body[1].data.children[0].data.distinguished;
 
-				// If the top comment is a mod comment, just check the next comment.
-				let index = 0;
-				if (checkForDistinguished === "moderator") index += 1;
+					// If the top comment is a mod comment, just check the next comment.
+					let index = 0;
+					if (checkForDistinguished === "moderator") index += 1;
 
-				const comments = body[1].data.children[index].data;
+					const comments = body[1].data.children[index].data;
 
-				// If the comment isn't getting 0.8 upvotes a minute, continue.
-				const commentAge = (seconds - comments.created_utc) / 60;
-				if (comments.ups / commentAge < minUpvotesPerMin) {
+					// If the comment isn't getting 0.8 upvotes a minute, continue.
+					const commentAge = (seconds - comments.created_utc) / 60;
+					if (comments.ups / commentAge < minUpvotesPerMin) {
+						continue;
+					}
+
+					const checkForReplies = comments.replies;
+					// Checks the comment or the first reply for replies, if there are none, push the link.
+					if (
+						checkForReplies === "" ||
+						checkForReplies.data.children[0].data.replies === ""
+					) {
+						qualifiedPost.push(post);
+					}
+				} catch (error) {
 					continue;
-				}
-
-				const checkForReplies = comments.replies;
-				// Checks the comment or the first reply for replies, if there are none, push the link.
-				if (
-					checkForReplies === "" ||
-					checkForReplies.data.children[0].data.replies === ""
-				) {
-					qualifiedPost.push(post);
 				}
 			}
 
