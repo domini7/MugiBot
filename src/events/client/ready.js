12ba4cc1a970d1aba4a7788be3644949c33e3262
Dominic Lamo <dominic.holguin7@gmail.com>
2021-10-11 20:25:38 -0700

    .

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,7 @@
 var colors = require("colors/safe");
 
 module.exports = (Discord, client) => {
-	console.log(colors.red("MugiBot is now on."));
+	console.log(colors.red("MugiBot is now on"));
 	console.log(colors.yellow("Servers: " + client.guilds.cache.size));
 	client.user.setActivity(";help");
 };
